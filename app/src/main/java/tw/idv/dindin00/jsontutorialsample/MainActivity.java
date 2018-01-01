72b06165fa037cd0d4b0a445522063b74872fb32
LI-ZHI_CHEN <Dindin5200@gmail.com>
2018-01-02 01:59:19 +0800

    各個變數進行初始化

--- app/src/main/java/tw/idv/dindin00/jsontutorialsample/MainActivity.java
+++ app/src/main/java/tw/idv/dindin00/jsontutorialsample/MainActivity.java
@@ -3,8 +3,14 @@ package tw.idv.dindin00.jsontutorialsample;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,5 +31,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //初始化requestQueue，注意此處不用new產生
+        requestQueue = Volley.newRequestQueue(this);
+        //初始化網址，此處採用新北市政府新聞局的新北市電影院名冊
+        jsonUrl = "http://data.ntpc.gov.tw/api/v1/rest/datastore/382000000A-000132-002";
+        //初始化jsonTAG，方便後面快速標記
+        jsonTAG = "getOpenData";
+        //初始化jsonObjectRequest，並進行json解析
+        jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, jsonUrl, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                //初始化結果文字確保每次內容不會混雜在一起
+                result = "";
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
     }
 }
