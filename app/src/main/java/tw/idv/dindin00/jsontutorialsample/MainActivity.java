92caae0eb4e82992409f8507ad8f5c643256b271
LI-ZHI_CHEN <Dindin5200@gmail.com>
2018-01-02 01:45:57 +0800

    修正變數宣告錯誤及更改註解

--- app/src/main/java/tw/idv/dindin00/jsontutorialsample/MainActivity.java
+++ app/src/main/java/tw/idv/dindin00/jsontutorialsample/MainActivity.java
@@ -4,19 +4,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.android.volley.RequestQueue;
-import com.android.volley.toolbox.JsonRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 
 public class MainActivity extends AppCompatActivity {
 
-    //volley用來處理網路連線工作的佇列
+    // volley用來處理網路連線工作的佇列
     RequestQueue requestQueue;
     //目標json的網址
     String jsonUrl;
-    //volley用來要求json資料的類別
-    JsonRequest jsonRequest;
-    //用來標記我們的網路連線工作
+    // volley用來要求json資料的類別，
+    // 由於本次範例資料最外層是jsonObject故採用此，
+    // 若是jsonArray則改JsonArrayRequest即可
+    JsonObjectRequest jsonObjectRequest;
+    // 用來標記我們的網路連線工作
     String jsonTAG;
-    //最後顯示的結果文字
+    // 最後顯示的結果文字
     String result;
 
     @Override
