2a7323b999ca0864cd69ea0e937fb58b2761ee85
LI-ZHI_CHEN <Dindin5200@gmail.com>
2018-01-02 01:29:10 +0800

    宣告變數

--- app/src/main/java/tw/idv/dindin00/jsontutorialsample/MainActivity.java
+++ app/src/main/java/tw/idv/dindin00/jsontutorialsample/MainActivity.java
@@ -3,7 +3,21 @@ package tw.idv.dindin00.jsontutorialsample;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonRequest;
+
 public class MainActivity extends AppCompatActivity {
+
+    //volley用來處理網路連線工作的佇列
+    RequestQueue requestQueue;
+    //目標json的網址
+    String jsonUrl;
+    //volley用來要求json資料的類別
+    JsonRequest jsonRequest;
+    //用來標記我們的網路連線工作
+    String jsonTAG;
+    //最後顯示的結果文字
+    String result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
